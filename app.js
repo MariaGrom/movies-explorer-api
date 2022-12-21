c5206f52767ca6f8d5054305394dae6cbe434ca7
Maria Gromyko <mazvereva91@mail.ru>
2022-12-21 18:59:33 +0300

    fix: error

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import helmet from 'helmet';
-import path from 'path';
 import { errors } from 'celebrate';
 import { router } from './routes/index.js';
 import { requestLogger, errorLogger } from './middlewares/logger.js';
@@ -12,20 +11,16 @@ import { errorHandler } from './middlewares/errorHandler.js';
 
 const app = express();
 
-const { PORT = 3003 } = process.env;
+const { PORT = 3003, NODE_ENV = 'development' } = process.env;
 
 // Определяем какой секретный ключ выбираем при продакшене
-const config = dotenv.config({
-  path: path
-    .resolve(process.env.NODE_ENV === 'production' ? '.env' : '.env.common'),
-})
-  .parsed;
+const config = dotenv.config({ path: NODE_ENV === 'production' ? '.env' : '.env.common' }).parsed;
 
 app.set('config', config);
 
 mongoose.set({ runValidators: true });
 mongoose.set('strictQuery', false);
-mongoose.connect(config.DB_URL); // подключаемся к базе данных
+mongoose.connect('mongodb://localhost:27017/moviesdb'); // подключаемся к базе данных
 
 app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
